9d31af4b8e122fcf0a99a71e3f052a62db55ece9
241933 <dmasdza@gmail.com>
2023-02-22 11:21:29 -0800

    0. _putchar

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void print_aphabet(void);
+void print_aphabet_x10(void);
+int _islower(int c);
+int _isapha(int C);
+int print_sign(int n);
+int _abs(int c);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+int _putchar(char c);
+
+#endif
